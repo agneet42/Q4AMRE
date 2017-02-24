7fcc908320b310dda02f2d7173c7ae380ac5a08c
Agneet Chatterjee <agneet257@gmail.com>
2017-02-25 01:11:52 +0530

    Update typ_question_type.py

--- typ_question_type.py
+++ typ_question_type.py
@@ -1,20 +1,4 @@
-#******************************************************************************
 
-#                         typ_question_type.py
-
-#                     Finds the type of a question
-
-# The type is chosen from a fixed list. For each type we have a predefined
-# means of answering it.
-# 
-
-#******************************************************************************
-
-#------------------------------------------------------------------------------
-# 
-# tokens is the query tokenised and parse is the query parsed. Determines the
-# type of the question, chosen from a fixed length.
-# 
 # This is just a placeholder.
 
 def typ_question_type( tokens, parse ):
